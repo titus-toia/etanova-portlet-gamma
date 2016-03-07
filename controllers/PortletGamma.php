0f0f69626ce495145cd5cf1caa4abf1394c92412
Titus Toia <titustoia@gmail.com>
2016-03-07 16:13:59 +0200

    first commit

---
+++ controllers/PortletGamma.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Etanova\controllers\portlets;
+use Etanova\Framework\controllers\portlets\BasePortlet;
+
+class PortletGamma extends BasePortlet {
+
+	public function CreateModel() {
+	}
+
+}
